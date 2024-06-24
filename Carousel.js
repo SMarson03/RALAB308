f2aabe3b82ee3644ed0cddc732168fe49e6d5901
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-23 20:32:58 -0400

    3

--- Carousel.js
+++ Carousel.js
@@ -1,25 +1,25 @@
-import * as bootstrap from "bootstrap";
-import { favourite } from "./index.js";
+// import * as bootstrap from "bootstrap";
+// import { favourite } from "./index.js";
 
-export function createCarouselItem(imgSrc, imgAlt, imgId) {
-  const template = document.querySelector("#carouselItemTemplate");
-  const clone = template.content.firstElementChild.cloneNode(true);
+// export function createCarouselItem(imgSrc, imgAlt, imgId) {
+//   const template = document.querySelector("#carouselItemTemplate");
+//   const clone = template.content.firstElementChild.cloneNode(true);
 
-  const img = clone.querySelector("img");
-  img.src = imgSrc;
-  img.alt = imgAlt;
+//   const img = clone.querySelector("img");
+//   img.src = imgSrc;
+//   img.alt = imgAlt;
 
-  const favBtn = clone.querySelector(".favourite-button");
-  favBtn.addEventListener("click", () => {
-    favourite(imgId);
-  });
+//   const favBtn = clone.querySelector(".favourite-button");
+//   favBtn.addEventListener("click", () => {
+//     favourite(imgId);
+//   });
 
-  return clone;
-}
+//   return clone;
+// }
 
-export function clear() {
-  const carousel = document.querySelector("#carouselInner");
-  while (carousel.firstChild) {
-    carousel.removeChild(carousel.firstChild);
-  }
-}
+// export function clear() {
+//   const carousel = document.querySelector("#carouselInner");
+//   while (carousel.firstChild) {
+//     carousel.removeChild(carousel.firstChild);
+//   }
+// }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const catAPIUrl = 'https://api.thecatapi.com/v1/'
+//Create an async function "initialLoad" that does the following:
 async function initialLoad(){
    try{
-    const response = await fetch(`${}breeds`);
+    const response = await fetch(`${thecatapi}breeds`);
     const breeds = await response.json ();
 
     const breedSelect = document.getElementById(`breedSelect`);
@@ -13,6 +15,11 @@ async function initialLoad(){
         option.textContent = breed.name;
         breedSelect.appendChild(option);
     });
-    
-}   
+    //call th event handler for breedselect to populate carousel and inforDump for the initial breed
+breedSelect.addEventListener(`change` , breedSelectHandler);
+breedSelect.dispatchEvent(new Event(`change`));
+}   catch (error) {
+    console.error(`Error fetchig breeds:` , error);
 }
+}
+console.log(initialLoad())
